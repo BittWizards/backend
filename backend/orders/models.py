5eb8d151f52199e2afb26d9681d8f6c64cbdcd97
Pavel Drovnin <pavel98rus@gmail.com>
2024-02-29 05:13:47 +0300

    refractoring order model

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,7 +1,8 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 
-from ambassadors.models import Ambassador
+from ambassadors.models import (Ambassador, AbstractAmbassador,
+                                AbstractAmbassadorAddress)
 from ambassadors.choices import AmbassadorsFootsSizes, AmbassadorsClothesSizes
 
 
@@ -25,33 +26,35 @@ class Merch(models.Model):
         ],
         default=0,
     )
+    size = models.CharField(
+        verbose_name="Размер для одежды",
+        null=True,
+        blank=True,
+        choices=(
+            AmbassadorsClothesSizes.choices + AmbassadorsFootsSizes.choices
+        )
+    )
 
     class Meta:
         verbose_name = "Мерч"
         verbose_name_plural = "Мерч"
-        ordering = ("name",)
+        ordering = ("name", "size")
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} ({self.size})"
 
 
-class Order(models.Model):
+class Order(AbstractAmbassador, AbstractAmbassadorAddress):
     """Модель для заявки на мерч"""
-    ambassador = models.ForeignKey(
+    ambassador_id = models.ForeignKey(
         Ambassador,
-        verbose_name="Получатель (амбассадор)",
+        verbose_name="ID амбассадора",
         on_delete=models.CASCADE,
     )
     merch = models.ManyToManyField(
         Merch,
         related_name="in_order",
         verbose_name="Мерч в заявке",
-    )
-    merch_size = models.CharField(
-        verbose_name="Размер для одежды",
-        null=True,
-        blank=True,
-        choices=(AmbassadorsClothesSizes.choices + AmbassadorsFootsSizes.choices)
     )
     order_status = models.CharField(
         verbose_name="Статус заявки",
@@ -74,10 +77,24 @@ class Order(models.Model):
         null=True,
         blank=True
     )
-    comment = models.CharField(
+    comment = models.TextField(
         verbose_name="Комментарий к заявке",
         null=True,
         blank=True
+    )
+    total_cost = models.IntegerField(
+        verbose_name="Полная стоимость продукции в заявке",
+        validators=[
+            MinValueValidator(0, "Стоимость не может быть отрицательной")
+        ],
+        null=True,
+        blank=True,
+    )
+    email = models.CharField(
+        verbose_name="Электронная почта", max_length=200
+    )
+    tg_acc = models.CharField(
+        verbose_name="Телеграмм аккаунт", max_length=150
     )
 
     class Meta:
@@ -86,5 +103,7 @@ class Order(models.Model):
         ordering = ("id",)
 
     @property
-    def total_cost(self):
-        return sum(merch.get('cost', 0) for merch in self.merch.values())
+    def get_full_address(self):
+        return '{}:{}:{}:{}'.format(
+            self.country, self.city, self.street_home, self.post_index
+        )
