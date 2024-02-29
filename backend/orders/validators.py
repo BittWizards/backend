ef9d722d824be15132b12ba1878757e35561570a
Pavel Drovnin <pavel98rus@gmail.com>
2024-02-29 07:15:13 +0300

    status validator

--- backend/orders/validators.py
+++ backend/orders/validators.py
@@ -1,5 +1,7 @@
-from rest_framework.exceptions import ValidationError
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
+
+from orders.models import OrderStatus
 
 
 def validate_merch_num(merch: list[dict]) -> None:
@@ -13,4 +15,13 @@ def validate_merch_num(merch: list[dict]) -> None:
         raise ValidationError(
             'Нельзя выбрать больше 3-х видов мерча',
             code=HTTP_400_BAD_REQUEST
+        )
+
+
+def validate_editing_order(order_status: OrderStatus) -> None:
+    """Проверка что заявку можно изменить"""
+    if order_status != OrderStatus.CREATED:
+        raise PermissionDenied(
+            'Поля заявки уже нельзя изменить',
+            code=HTTP_403_FORBIDDEN
         )
