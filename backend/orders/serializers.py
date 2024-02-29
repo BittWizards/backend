b45de89185fdc4ab8dfd54c0cfcc35a487450798
Pavel Drovnin <pavel98rus@gmail.com>
2024-02-29 05:25:14 +0300

    add get, list, post orders func

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 
-
-from .models import Merch, Order
-from ambassadors.serializers import AmbassadorSerializer
+from orders.models import Merch, Order
+from orders.validators import validate_merch_num
 
 
 class MerchSerializer(serializers.ModelSerializer):
@@ -10,15 +9,20 @@ class MerchSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Merch
-        fields = ('__all__')
+        fields = ('name', 'size')
 
 
 class OrderSerializer(serializers.ModelSerializer):
     """Сериалайзер для заявок на мерч"""
-    ambassador = AmbassadorSerializer()
-    merch = MerchSerializer(many=True)
+    merch = MerchSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = ('__all__')
-        read_only_fields = ('created_date', 'order_status')
+        read_only_fields = ('ambassador_id', 'created_date', 'order_status')
+
+    def validate(self, attrs):
+        merch = self.initial_data['merch']
+        validate_merch_num(merch)
+        attrs['merch'] = merch
+        return attrs
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import HTTP_201_CREATED
+from django.shortcuts import get_object_or_404
+from django.db.models import Q, QuerySet
 from drf_spectacular.utils import extend_schema_view
 
 from openapi.orders_schema import (orders_extend_schema_view,
                                    merch_extend_schema_view)
-from .models import Merch, Order
-from .serializers import (
+from orders.models import Merch, Order
+from ambassadors.models import Ambassador
+from orders.serializers import (
     OrderSerializer,
     MerchSerializer
 )
@@ -15,8 +22,46 @@ class OrderViewSet(viewsets.ModelViewSet):
     """ViewSet для заявок на мерч"""
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_queryset(self) -> QuerySet[Order]:
+        ambassador_id = int(self.kwargs.get('pk'))
+        query = Order.objects.filter(ambassador_id=ambassador_id)
+        return query
+
+    def get_object(self) -> Order:
+        order = get_object_or_404(Order, pk=int(self.kwargs.get('pk')))
+        return order
+
+    def create(self, request: Request, *args, **kwargs) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        merch = Merch.objects.filter(
+            Q(name__in=(merch.get('name') for merch in request.data['merch'])),
+            Q(
+                size__in=(merch.get('size') for merch in request.data['merch'])
+            ) | Q(size__isnull=True)
+        )
+        self.perform_create(serializer, merch)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=HTTP_201_CREATED, headers=headers
+        )
+
+    def perform_create(self, serializer: Serializer, merch: Merch) -> None:
+        ambassador = get_object_or_404(
+            Ambassador, pk=self.kwargs['pk']
+        )
+        serializer.validated_data['ambassador_id'] = ambassador
+        serializer.save(merch=merch)
+
+    def partial_update(self, request: Request, *args, **kwargs) -> Response:
+        return super().partial_update(request, *args, **kwargs)
 
     # TODO: Если добавить трек -> изменить статус
+    # TODO: Изменение адрес в заявке
+    # TODO: Если статус отправлен -> нельзя изменять заявку
+    # TODO: Проверка на макс количества мерча в завке
 
 
 @extend_schema_view(**merch_extend_schema_view)
@@ -24,3 +69,4 @@ class MerchViewSet(viewsets.ModelViewSet):
     """ViewSet для мерча"""
     queryset = Merch.objects.all()
     serializer_class = MerchSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
