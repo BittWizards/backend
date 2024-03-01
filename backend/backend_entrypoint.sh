120100d2c632e38bae968f9e4176dd66e9ddb2e5
Pavel Drovnin <pavel98rus@gmail.com>
2024-03-01 06:44:12 +0300

    undo infra, dc file, entrypoint

--- backend/backend_entrypoint.sh
+++ backend/backend_entrypoint.sh
@@ -5,13 +5,11 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-cp -r collected_static/. /collected_static/
+cp -r collected_static/. /backend_static/static/
 
 DJANGO_SUPERUSER_PASSWORD=admin \
 DJANGO_SUPERUSER_USERNAME=admin \
 DJANGO_SUPERUSER_EMAIL="admin@admin.com" \
 python manage.py createsuperuser --noinput
-
-python manage.py loaddata */fixtures/*.json
 
 gunicorn --reload -b 0.0.0.0:8000 ambassadors_project.wsgi
