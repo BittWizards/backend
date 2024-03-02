9f8ffe79e9ffcf860f1e9451ab752315678bf1ef
Andrey Varachev <Dartanyun@yandex.com>
2024-03-03 01:13:41 +0500

    Переписан AmbassadorViewSet
    Теперь основная логика операций create и update перенесена в serializer

--- backend/ambassadors/views.py
+++ backend/ambassadors/views.py
@@ -1,15 +1,14 @@
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import status
+
+from content.mixins import CreateRetrieveListViewSet
 
 from .models import Ambassador
 from .serializers import AmbassadorListSerializer, AmbassadorSerializer
 
 
-class AmbassadorViewSet(viewsets.ModelViewSet):
+class AmbassadorViewSet(CreateRetrieveListViewSet):
     """
     Viewset модели Ambassador.
     """
@@ -18,16 +17,14 @@ class AmbassadorViewSet(viewsets.ModelViewSet):
     serializer_class = AmbassadorListSerializer
     permission_classes = (AllowAny,)
 
-    @action(
-        methods=("get",),
-        url_path=r"(?P<pk>\d+)",
-        detail=False,
-        permission_classes=(AllowAny,),
-    )
-    def ambassadors_list(self, request, pk):
-        try:
-            ambassador = Ambassador.objects.get(id=pk)
-            serializer = AmbassadorSerializer(ambassador)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except ObjectDoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self, request, pk):
+        queryset = Ambassador.objects.all()
+        ambassador = get_object_or_404(queryset, pk=pk)
+        serializer = AmbassadorSerializer(ambassador)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AmbassadorListSerializer
+        else:
+            return AmbassadorSerializer
