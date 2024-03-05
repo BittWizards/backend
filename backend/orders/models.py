1ce793397deb1ead0562885f7823cb0fa52ea232
Pavel Drovnin <pavel98rus@gmail.com>
2024-03-05 03:25:56 +0300

    isort

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -2,10 +2,7 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 from ambassadors.choices import AmbassadorsClothesSizes, AmbassadorsFootsSizes
-from ambassadors.models import (
-    AbstractAmbassadorAddress,
-    Ambassador,
-)
+from ambassadors.models import AbstractAmbassadorAddress, Ambassador
 from users.models import AbstractUser
 
 
