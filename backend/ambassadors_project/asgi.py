98df8ed2fccd7e2daf2d8b7a1c1ba7091dce92d6
Андрей Варачев <dartanyun@yandex.com>
2024-03-07 00:22:18 +0500

    ?
    без этого не отправлялись пост запросы на локалке

--- backend/ambassadors_project/asgi.py
+++ backend/ambassadors_project/asgi.py
@@ -9,23 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-import websocket.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ambassadors_project.settings")
 
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter(
-    {
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(websocket.routing.websocket_urlpatterns)
-            )
-        ),
-    }
-)
+application = get_asgi_application()
