30cbfa5a98af2908b7c4f778e2b8f54ac5430240
Андрей Варачев <Dartanyun@yandex.com>
2024-03-07 14:22:51 +0500

    Обновление полей моделей

--- backend/ambassadors/models.py
+++ backend/ambassadors/models.py
@@ -73,8 +73,10 @@ class Ambassador(AbstractUser):
     education = models.CharField(verbose_name="Образование", max_length=300)
     work = models.CharField(verbose_name="Работа", blank=True, null=True)
     status = models.CharField(
-        verbose_name="Статус", max_length=50, choices=AmbassadorStatus.choices,
-        default=AmbassadorStatus.CLARIFY
+        verbose_name="Статус",
+        max_length=50,
+        choices=AmbassadorStatus.choices,
+        default=AmbassadorStatus.CLARIFY,
     )
     created = models.DateTimeField(
         verbose_name="Дата и время создания", default=timezone.now
@@ -92,7 +94,10 @@ class Ambassador(AbstractUser):
         default="profiles/default_pic.jpeg",
     )
     extra_info = models.CharField(
-        verbose_name="Дополнительная информация", blank=True, null=True, max_length=2000
+        verbose_name="Дополнительная информация",
+        blank=True,
+        null=True,
+        max_length=2000,
     )
 
     class Meta:
